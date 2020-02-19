407205219e5c892aac1309b5098a9c1e2f5580d0
Elisa-Manzano <55851498+Elisa-Manzano@users.noreply.github.com>
2020-02-19 13:55:06 -0600

    Add files via upload

---
+++ saludo.py
@@ -0,0 +1,8 @@
+#Escribe un programa que solicite un nombre e imprima un saludo añadiendo el
+#nombre dado, el archivo se llamará iniciales_hello_word.py.
+
+
+print('¡Hola mundo!')
+print('¿Cómo te llamas?')
+miNombre = input()
+print('Es un placer conocerte, ' + miNombre)
