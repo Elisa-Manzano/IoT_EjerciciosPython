66b9ec5bd234d04a3747ce42e7c7ff452c11ae3b
Elisa-Manzano <55851498+Elisa-Manzano@users.noreply.github.com>
2020-02-19 13:54:45 -0600

    Add files via upload

---
+++ TablasMul.py
@@ -0,0 +1,16 @@
+
+#Función que imprima las 10 tablas de multiplicar.
+
+def imprimir_tabla(numero):
+    # Se supone que las tablas llegan hasta el 10
+    LIMITE = 10
+    # Comenzar en 1
+    contador = 1
+    while contador <= LIMITE:
+        resultado = contador * numero
+        print("{} x {} = {}".format(numero, contador, resultado))
+        # Incrementar contador para no caer en ciclo infinito
+        contador = contador + 1
+
+# Probar función
+imprimir_tabla(4)
