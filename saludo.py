7dfd84605de8fa3a401de20c3c9b9c6a809d8828
Elisa-Manzano <55851498+Elisa-Manzano@users.noreply.github.com>
2020-02-19 23:57:40 -0600

    Add files via upload

--- saludo.py
+++ saludo.py
@@ -1,3 +1,7 @@
+
+#Aplicaciones De IoT
+#Balderas Manzano Maria Elisa
+#GDS0151, 19 de febrero 2020
 #Escribe un programa que solicite un nombre e imprima un saludo añadiendo el
 #nombre dado, el archivo se llamará iniciales_hello_word.py.
 
