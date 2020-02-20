404fc332432ab1ea60f2676b7419770e27c5fc76
Elisa-Manzano <55851498+Elisa-Manzano@users.noreply.github.com>
2020-02-19 23:58:01 -0600

    Add files via upload

--- Sumatoria.py
+++ Sumatoria.py
@@ -1,3 +1,6 @@
+#Aplicaciones De IoT
+#Balderas Manzano Maria Elisa
+#GDS0151, 19 de febrero 2020
 #Función que cree solicite 10 valores de una lista y haga la sumatoria de los mismos
 #e imprima.
 def sumalista(listaNumeros):
@@ -5,5 +8,5 @@ def sumalista(listaNumeros):
     for i in listaNumeros:
         laSuma = laSuma + i
     return laSuma
-
+#Ingrese Numeros a sumar
 print(sumalista([1,1,1,1,1,1,1,1,1,1]))
