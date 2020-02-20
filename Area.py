a17d2eac961cb491e534652c894709683c6a2a12
Elisa-Manzano <55851498+Elisa-Manzano@users.noreply.github.com>
2020-02-19 23:56:16 -0600

    Add files via upload

--- Area.py
+++ Area.py
@@ -1,4 +1,6 @@
-
+#Aplicaciones De IoT
+#Balderas Manzano Maria Elisa
+#GDS0151, 19 de febrero 2020
 #Crea una función que calcule el área del triángulo.
 print ("CALCULAR EL AREA DE UN TRIANGULO")
 base=input("CUAL ES LA BASE: ")
